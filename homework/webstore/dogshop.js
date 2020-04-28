81587e80c6e9b6d29537aef85cc1e708da81e6e7
Scept15 <scepterolegario@gmail.comgit config --global user.email scepterolegario@gmail.com>
2020-04-28 18:45:54 +0800

    Dogshop

---
+++ homework/webstore/dogshop.js
@@ -0,0 +1,42 @@
+// 1st slider
+$('.slider-one').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+
+
+
+
+
+
+// $('.slider-one')
+// .not(".slick-intialized")
+// .slick({
+//     autoplay:true,
+//     autoplay:3000,
+//     dots:true,
+//     prevArrow:".site-slider .slider-btn .prev",
+//     nextArrow:".site-slider .slider-btn .next",
+// });
