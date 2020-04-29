46320fe76b1f78fc9c8e50642a597b303dfb21e4
Scept15 <scepterolegario@gmail.comgit config --global user.email scepterolegario@gmail.com>
2020-04-29 18:26:35 +0800

    dogshop

--- homework/webstore/dogshop.js
+++ homework/webstore/dogshop.js
@@ -1,29 +1,29 @@
 // 1st slider
-$('.slider-one').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
+// $('.slider-one').slick({
+//     centerMode: true,
+//     centerPadding: '60px',
+//     slidesToShow: 3,
+//     responsive: [
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           arrows: false,
+//           centerMode: true,
+//           centerPadding: '40px',
+//           slidesToShow: 3
+//         }
+//       },
+//       {
+//         breakpoint: 480,
+//         settings: {
+//           arrows: false,
+//           centerMode: true,
+//           centerPadding: '40px',
+//           slidesToShow: 1
+//         }
+//       }
+//     ]
+//   });
 
 
 
@@ -31,12 +31,12 @@ $('.slider-one').slick({
 
 
 
-// $('.slider-one')
-// .not(".slick-intialized")
-// .slick({
-//     autoplay:true,
-//     autoplay:3000,
-//     dots:true,
-//     prevArrow:".site-slider .slider-btn .prev",
-//     nextArrow:".site-slider .slider-btn .next",
-// });
+$(".slider-one")
+.not(".slick-intialized")
+.slick({
+    autoplay:true,
+    autoplay:3000,
+    dots:true,
+    prevArrow:".site-slider .slider-btn .prev",
+    nextArrow:".site-slider .slider-btn .next",
+});
